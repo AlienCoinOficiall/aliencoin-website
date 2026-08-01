1e87332cac84554186e9f111e8b2292e13f726b4
AlienCoinOficiall <hendritofalentino@gmail.com>
2026-08-01 23:40:53 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -601,22 +601,3 @@ console.log(
 
 );
 
-/* ==========================================
-   HEADER SCROLL EFFECT
-========================================== */
-
-const header = document.querySelector("header");
-
-window.addEventListener("scroll", () => {
-
-    if (window.scrollY > 50) {
-
-        header.classList.add("scrolled");
-
-    } else {
-
-        header.classList.remove("scrolled");
-
-    }
-
-});
